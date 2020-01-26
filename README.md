0453e836e429bf97e2e74ff13b671d31bf2375f4
BKJang <jp302119@gmail.com>
2020-01-26 16:00:03 +0900

    init create-react-hook@3.0.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # use-lazyload
 
-> custom hooks for lazyloding in react.
+> 
 
 [![NPM](https://img.shields.io/npm/v/use-lazyload.svg)](https://www.npmjs.com/package/use-lazyload) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,23 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import * as React from 'react';
 
-interface IObserverOptions {
-  root: Element | null;
-  rootMargin: string;
-  thresholds: ReadonlyArray<number>;
-}
+export const useMyHook = () => {
+  let [{
+    counter
+  }, setState] = React.useState<{
+    counter: number;
+  }>({
+    counter: 0
+  });
 
-const defaultOptions: IObserverOptions = {
-  root: null,
-  rootMargin: "0px",
-  thresholds: [0]
-};
-
-export const useLazyload = (onIntersectCallbck: Function, options = defaultOptions) => {
-  const [element, setElement] = useState(null);
-  const doIntersectCallback = useCallback((entry, observer) => {
-    if (entry.isIntersecting) {
-      onIntersectCallbck(entry, observer);
-    }
+  React.useEffect(() => {
+    let interval = window.setInterval(() => {
+      counter++;
+      setState({counter})
+    }, 1000)
+    return () => {
+      window.clearInterval(interval);
+    };
   }, []);
 
-  useEffect(() => {
-    let observer: any;
-    if (element) {
-      observer = new IntersectionObserver(doIntersectCallback, {
-        ...options
-      });
-      observer.observe(element);
-    }
-    return () => observer && observer.disconnect();
-  }, [element, options, onIntersectCallbck]);
-
-  return [element, setElement];
+  return counter;
 };
