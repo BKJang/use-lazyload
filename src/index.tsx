dbbffdb1897c1ca417815422d6bb706e6d80192b
BKJang <jp302119@gmail.com>
2020-01-26 16:45:40 +0900

    📚Modify use-lazyload

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,36 @@
-import * as React from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
-  });
+interface IObserverOptions {
+  root: Element | null;
+  rootMargin: string;
+  thresholds: ReadonlyArray<number>;
+};
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
-    };
+const defaultOptions: IObserverOptions = {
+  root: null,
+  rootMargin: "0px",
+  thresholds: [0]
+};
+
+export const useLazyload = (onIntersectCallbck: Function, options = defaultOptions) => {
+  const [element, setElement] = useState(null);
+  const doIntersectCallback = useCallback(([entry], observer) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      onIntersectCallbck(entry, observer);
+    }
   }, []);
 
-  return counter;
+  useEffect(() => {
+    let observer: any;
+    if (element) {
+      observer = new IntersectionObserver(doIntersectCallback, {
+        ...options
+      });
+      observer.observe(element);
+    }
+    return () => observer && observer.disconnect();
+  }, [element, options, onIntersectCallbck]);
+
+  return [element, setElement];
 };
