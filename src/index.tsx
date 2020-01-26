ccabd4ed1beced401eb32da653a727ed4c671e63
BKJang <jp302119@gmail.com>
2020-01-26 15:31:48 +0900

    📚Implement useLazyload without polyfill

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,33 @@ import React, { useState, useEffect, useCallback } from 'react';
 interface IObserverOptions {
   root: Element | null;
   rootMargin: string;
-  thresholds: Array<number>;
+  thresholds: ReadonlyArray<number>;
 }
 
 const defaultOptions: IObserverOptions = {
   root: null,
   rootMargin: "0px",
   thresholds: [0]
+};
+
+export const useLazyload = (onIntersectCallbck: Function, options = defaultOptions) => {
+  const [element, setElement] = useState(null);
+  const doIntersectCallback = useCallback((entry, observer) => {
+    if (entry.isIntersecting) {
+      onIntersectCallbck(entry, observer);
+    }
+  }, []);
+
+  useEffect(() => {
+    let observer: any;
+    if (element) {
+      observer = new IntersectionObserver(doIntersectCallback, {
+        ...options
+      });
+      observer.observe(element);
+    }
+    return () => observer && observer.disconnect();
+  }, [element, options, onIntersectCallbck]);
+
+  return [element, setElement];
 };
