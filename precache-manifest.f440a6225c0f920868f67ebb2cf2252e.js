c9cd984c833b8543656d9ee2d2967543815be13e
BKJang <jp302119@gmail.com>
2020-01-26 18:37:47 +0900

    Updates

---
+++ precache-manifest.f440a6225c0f920868f67ebb2cf2252e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a58015add0902c24ad21cc6a6721d2",
+    "url": "/use-lazyload/index.html"
+  },
+  {
+    "revision": "7ee301b39af87f35a5a1",
+    "url": "/use-lazyload/static/css/main.608d28f9.chunk.css"
+  },
+  {
+    "revision": "46669ee71b3626e4987f",
+    "url": "/use-lazyload/static/js/2.99f4bda5.chunk.js"
+  },
+  {
+    "revision": "d50198c2b3cb3b9437d9698faa7c978c",
+    "url": "/use-lazyload/static/js/2.99f4bda5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ee301b39af87f35a5a1",
+    "url": "/use-lazyload/static/js/main.eceb67db.chunk.js"
+  },
+  {
+    "revision": "c9a880b8bd4c4bbbf115",
+    "url": "/use-lazyload/static/js/runtime-main.230ce336.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/use-lazyload/precache-manifest.f440a6225c0f920868f67ebb2cf2252e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/use-lazyload/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
